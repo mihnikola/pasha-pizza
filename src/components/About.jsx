1809a67ae1fce6af7a6109da1b1c8de4ecd74d11
mihnikola <47032376+mihnikola@users.noreply.github.com>
2024-12-10 16:37:14 +0100

    Add files via upload

---
+++ src/components/About.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="min-h-[600px] bg-gray-700 flex flex-col items-end justify-center mt-14 bg-[url('./assets/img/aboutUs.png')] bg-cover bg-no-repeat">
+      <div className=" mx-auto mr-44">
+        <h3 className=" text-white text-5xl pr-16 font-semibold">The Best</h3>
+        <h1 className=" text-yellow-400 text-7xl md:text-9xl font-bold">PIZZA</h1>
+        <h3 className=" text-white text-6xl pl-20">Around</h3>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Review.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import ReviewCard from "../layouts/ReviewCard";
+import img1 from "../assets/img/pic1.png";
+import img2 from "../assets/img/pic2.png";
+import img3 from "../assets/img/pic3.png";
+
+const Review = () => {
+  return (
+    <div className="min-h-[600px] flex flex-col items-center justify-center md:px-32 px-5">
+      <h1 className=" text-4xl font-semibold text-center lg:pt-16 pt-24 pb-10">
+        {"Customer's Review"}
+      </h1>
+      <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <ReviewCard img={img1} name="Sophia Azura" description="I’ve been to many pizzerias, but none compare to this one! The crust is perfectly thin and crispy, the sauce is fresh and flavorful, and the toppings are always top-notch. You can really taste the quality of the ingredients. Whether you’re a classic Margherita lover or you like to experiment with different flavors, this pizzeria never disappoints." />
+        <ReviewCard img={img2} name="John Deo" description="This place serves the best pizza in town—hands down. The ingredients are always fresh, and the flavors are incredible. The staff is friendly, the atmosphere is welcoming, and they truly know how to make a pizza. I always leave satisfied and craving more. If you haven't tried it yet, do yourself a favor and pay them a visit!" />
+        <ReviewCard img={img3} name="Victoria Zoe" description="I’m a pizza enthusiast, and I can confidently say that this pizzeria is one of the best I’ve ever been to. From the first bite, you can tell that they care about quality. The variety of pizzas is amazing, and they have something for everyone. The cheese is perfectly melted, the sauce is rich and flavorful, and the toppings are always fresh." />
+      </div>
+    </div>
+  );
+};
+
+export default Review;
